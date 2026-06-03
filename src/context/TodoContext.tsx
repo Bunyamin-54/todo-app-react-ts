1bbd314eb04dd33ac3fa72e0265a3b58ef481ee7
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-06-03 22:26:10 +0200

    context api in react

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,12 +1,30 @@
-import React, {createContext} from "react"
-
- 
- const TodoContext = createContext(null);
- 
-
- export function TodoProvider({children}: {children:React.ReactNode}){
+import { createContext, useContext, useState } from "react";
+import type { ITodo } from "../ts/todos";
 
 
-  return <TodoContext.Provider value={{}}>{children} </TodoContext.Provider>
+interface ITodoContext {
+  todos: ITodo[];
+  setTodos: React.Dispatch<React.SetStateAction<ITodo[]>>;
+}
 
- }
+const TodoContext = createContext<ITodoContext |null >(null)
+
+export function TodoProvider({ children }: { children: React.ReactNode }) {
+  const [todos, setTodos] = useState<ITodo[]>([]);
+
+  return (
+    <TodoContext.Provider value={{ todos, setTodos }}>
+   
+      {children}
+    </TodoContext.Provider>
+  );
+}
+
+export function useTodoContext(TodoContext){
+
+
+  const context =  useContext(TodoContext)
+
+  if(!context)
+
+}
