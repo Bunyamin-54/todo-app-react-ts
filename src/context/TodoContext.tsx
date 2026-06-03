35f42f793f454ddf146d6b0225293f7183182cef
Bünyamin Genc <bnymngnc.1660@gmail.com>
2026-06-03 21:15:53 +0200

    provider

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -3,3 +3,10 @@ import {createContext} from "react"
  
  const TodoContext = createContext(undefined);
  
+
+ export function TodoProvider(){
+
+
+  return <TodoContext.Provider value={{}}> </TodoContext.Provider>
+
+ }
